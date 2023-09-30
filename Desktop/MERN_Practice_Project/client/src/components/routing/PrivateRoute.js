d2b55bc7ad4d6b8a2cdfef798863a354d30e51cb
Mengchen Ye <914864251@qq.com>
2023-09-30 10:50:41 +1000

    Initial commit

---
+++ Desktop/MERN_Practice_Project/client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const PrivateRoute = ({ children, auth: { isAuthenticated, loading }, ...rest }) => {
+  if (!isAuthenticated && !loading) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+  children: PropTypes.node.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
