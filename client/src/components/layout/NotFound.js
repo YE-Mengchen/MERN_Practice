abb7a37c1cac620f0950bc593310035d1e08f2ad
Mengchen Ye <914864251@qq.com>
2024-02-24 15:40:47 +1100

    build

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const NotFound = () => {
   return (
-    <section className="container">
+    <section className="text-5xl font-bold flex flex-col justify-center m-20 space-y-10">
       <h1 className="x-large text-primary">
-        <i className="fas fa-exclamation-triangle" /> Page Not Found
+        <i className="fas fa-exclamation-triangle " /> Page Not Found
       </h1>
       <p className="large">Sorry, this page does not exist</p>
     </section>
